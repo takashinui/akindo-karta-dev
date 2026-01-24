c8a86df162e92c0827ffdcc3d3d8fce921ec0aa7
takashinui <inui.takashi@inui.co.jp>
2026-01-24 12:38:27 +0900

    Update fetchNews.js

--- scripts/fetchNews.js
+++ scripts/fetchNews.js
@@ -1,1 +1,6 @@
-console.log("fetchNews.js is running");
+const RSS_URL = "https://www3.nhk.or.jp/rss/news/cat5.xml";
+
+const res = await fetch(RSS_URL);
+const text = await res.text();
+
+console.log(text.slice(0, 500));
